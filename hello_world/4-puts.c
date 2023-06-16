424cf8384fe1c22524a509cc6cd01fcd07e0f131
romeollangozi <romeollangozi123@gmail.com>
2023-06-16 02:44:24 -0700

    4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -9,7 +9,7 @@
 int main(void)
 {
 
-	char *string = "\"Programming is like building a multilingual puzzle\n";
+	char *string = "\"Programming is like building a multilingual puzzle";
 
 	puts(string);
 
