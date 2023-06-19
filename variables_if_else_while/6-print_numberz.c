219efcec36567dbc2abf9623b826447557b0b12a
romeollangozi <romeollangozi123@gmail.com>
2023-06-19 02:39:20 -0700

    6

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -7,9 +7,8 @@
 int main(void)
 {
 int i;
-char numbers[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-for (i = 0; i < 10; i++)
-putchar(numbers[i]);
+for (i = 48; i < 58; i++)
+putchar(i);
 putchar('\n');
 return (0);
 }
