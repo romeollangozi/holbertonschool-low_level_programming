e615bc35b5c5b22d6d3a1e6caec7e2173d1ce6e4
romeollangozi <romeollangozi123@gmail.com>
2023-06-19 02:47:14 -0700

    8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int c;
+for (c = 48; c < 58; ++c)
+{
+putchar(c);
+}
+for (c = 97; c < 103 ; ++c)
+{
+putchar(c);
+}
+printf("\n");
+return (0);
+
+}
