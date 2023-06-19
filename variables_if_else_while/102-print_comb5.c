898df0bc57676fc4521efcc42640142a3b8b565d
romeollangozi <romeollangozi123@gmail.com>
2023-06-19 06:29:39 -0700

    102

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -6,31 +6,34 @@
  */
 int main(void)
 {
-        int i, j, k, m;
-        for (i = 48; i < 58; i++)
-       
-	{
-                for (j = 48; j < 58 ; j++)
-                {
 
-                        for (k = 48; k < 58; k++)
-                        {
-                                for( m = 48; m < 58; m++)
-                                {
-                                  if (i+j < m + k)
-                                  {
-                                  putchar(i);
-                                  putchar(j);
-                                  putchar(' ');
-                                  putchar(k);
-                                  putchar(m);
-                                  putchar(',');
-                                  }
-                                }
-                        }
-                }
-        }
-        putchar('\n');
-        return (0);
+int i, j, k, m;
 
+for (i = 48; i < 58; i++)
+{
+for (j = 48; j < 58 ; j++)
+{
+
+for (k = 48; k < 58; k++)
+{
+for (m = 48; m < 58; m++)
+{
+if ((i + j < m + k) && (i + j != m + k))
+{
+putchar(i);
+putchar(j);
+putchar(' ');
+putchar(k);
+putchar(m);
+if (i+j+k+m <= 226){
+putchar(',');
+putchar(' ');
 }
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}       
