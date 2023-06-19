4b499ecd3f40085bf2d0d2e3b6be72bbc269213e
romeollangozi <romeollangozi123@gmail.com>
2023-06-19 03:22:09 -0700

    9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -7,10 +7,15 @@
 int main(void)
 {
 int i;
-for (i = 48; i < 58; i++){
+for (i = 48; i < 58; i++)
+{
 putchar(i);
+if (i < 57)
+{
 putchar(',');
 putchar(' ');
 }
+}
+putchar('\n');
 return (0);
 }
