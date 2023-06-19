fb8ab7538f41170479c29651e07413edb64a1fe1
romeollangozi <romeollangozi123@gmail.com>
2023-06-19 06:04:19 -0700

    102

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        int i, j, k, m;
+        for (i = 48; i < 58; i++)
+       
+	{
+                for (j = 48; j < 58 ; j++)
+                {
+
+                        for (k = 48; k < 58; k++)
+                        {
+                                for( m = 48; m < 58; m++)
+                                {
+                                  if (i+j < m + k)
+                                  {
+                                  putchar(i);
+                                  putchar(j);
+                                  putchar(' ');
+                                  putchar(k);
+                                  putchar(m);
+                                  putchar(',');
+                                  }
+                                }
+                        }
+                }
+        }
+        putchar('\n');
+        return (0);
+
+}
