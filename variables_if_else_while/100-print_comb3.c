0554cc387abcfc1f8ca83bdc73590fde2e4eb7b6
romeollangozi <romeollangozi123@gmail.com>
2023-06-19 04:06:46 -0700

    100

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i, j;
+for (i = 48; i < 58; i++)
+{
+for (j = 48; j < 58 ; j++)
+{
+if ((i != j) && (i < j))
+{
+putchar(i);
+putchar(j);
+if (i != 56)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+
+putchar('\n');
+return (0);
+
+}
