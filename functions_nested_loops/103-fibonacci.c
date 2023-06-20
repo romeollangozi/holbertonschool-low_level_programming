72020c5eca2b0ed129c698532a0b43fc54be98fa
romeollangozi <romeollangozi123@gmail.com>
2023-06-20 15:10:13 -0700

    103

---
+++ functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int i;
+long int a = 1;
+long int b = 1;
+long int t;
+long int sum;
+while (b <= 4000000)
+{
+t = b;
+b = b + a;
+a = t;
+if (t % 2)
+sum = sum + t;
+}
+printf("%li \n", sum);
+return (0);
+}
