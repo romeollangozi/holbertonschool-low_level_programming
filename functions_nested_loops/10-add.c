db6d7237cfe220e907e8147380763ce804efa01a
romeollangozi <romeollangozi123@gmail.com>
2023-06-20 06:32:08 -0700

    10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - adds two numbers together
+ * @a: first input
+ * @b: second input
+ * Return: returns the sum of the numbers
+ */
+
+int add(int a, int b)
+{
+return (a + b);
+}
