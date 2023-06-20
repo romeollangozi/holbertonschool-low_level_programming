8dc5257b176e9dd1150e863ced1a24d7e39694b1
romeollangozi <romeollangozi123@gmail.com>
2023-06-20 04:07:43 -0700

    5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _print_sign - it takes an argument c of value integer
+ * @n: integer
+ * Return: 1 and prints + if the number is greater than zero
+ * returns 0 and prints 0 if the number is 0
+ * returns -1 and prints - if the numbers is less than zero
+ */
+int _print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (1);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
+}
