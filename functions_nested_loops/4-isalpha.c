b102b5e0251019dde893943dab2e9557dc599799
romeollangozi <romeollangozi123@gmail.com>
2023-06-20 03:35:09 -0700

    4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - it takes an argument c of value integer
+ * @c: character
+ * Return: 1 if the character
+ * is a letter and returns 0 if not
+ */
+int _isalpha(int c)
+{
+if ((c >= 97 && c <= 122) || (c >= 65 && c >= 90))
+return (1);
+else
+return (0);
+}
