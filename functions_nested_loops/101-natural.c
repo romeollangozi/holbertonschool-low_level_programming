d3eaa5eb0d311903aeac8887613f2ad508e06cf8
romeollangozi <romeollangozi123@gmail.com>
2023-06-20 10:09:04 -0700

    101

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 
 int main(void)
 {
 int i;
 int sum;
-for( i = 3; i < 1024; i++)
+for (i = 3; i < 1024; i++)
 {
-if((i % 3 == 0) || (i % 5 == 0))
+if ((i % 3 == 0) || (i % 5 == 0))
 {
 
-   sum = sum + i;
+sum = sum + i;
 
 }
 
