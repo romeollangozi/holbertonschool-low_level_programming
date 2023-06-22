f370fbcfdf15b4fe032c1a1c7d3ae6e8206fc7ce
romeollangozi <romeollangozi123@gmail.com>
2023-06-22 01:45:15 -0700

    5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -12,13 +12,9 @@ void more_numbers(void)
 	{
 		while (j < 15)
 		{
-			if (j < 10)
-			_putchar('0' + j);
-			else
-			{
+			if (j >= 10)
 			_putchar('0' + j / 10);
 			_putchar('0' + j % 10);
-			}
 			j++;
 		}
 			_putchar('\n');
