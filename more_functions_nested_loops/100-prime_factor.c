b05596697ebdd9dfb9cd8177c6eb577115c6229c
romeollangozi <romeollangozi123@gmail.com>
2023-06-23 01:30:57 -0700

    100

--- more_functions_nested_loops/100-prime_factor.c
+++ more_functions_nested_loops/100-prime_factor.c
@@ -1,23 +1,5 @@
 #include <stdio.h>
-/**
- * is_prime - finds if the number is a prime or not
- * @n: length argument of the line being drawn
- * Return: returns 1 if its a prime number
- * returns 0 if not
- */
-int is_prime(long int n)
-{
-	int i;
-
-	for (i = 2; i < n; i++)
-	{
-		if (n % i == 0)
-			return (0);
-
-	}
-		return (1);
-}
-
+#include <math.h>
 /**
  * main - Entry point
  *
@@ -25,24 +7,19 @@ int is_prime(long int n)
  */
 int main(void)
 {
-
-	int a;
-	long int largest = 0;
-	int i;
+	long int i;
 	long int n = 612852475143;
+	long int f = 2;
 
-	for (i = 2; i < n; i++)
+	while (n > 1)
 	{
-		if (n % i == 0)
+		if (n % f == 0)
 		{
-			if (is_prime(i))
-			{
-				a = i;
-				if (a > largest)
-				largest = a;
-			}
+			n = n / f;
 		}
+		else
+			f = f + 1;
 	}
-	printf("%li\n", largest);
+	printf("%i", f);
 	return (0);
 }
