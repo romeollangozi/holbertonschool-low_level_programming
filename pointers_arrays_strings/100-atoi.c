8514b285cc4a487220c0995de48b05a25fa99b18
romeollangozi <romeollangozi123@gmail.com>
2023-06-26 08:52:02 -0700

    100

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -7,46 +7,35 @@
  */
 int _atoi(char *s)
 {
-	unsigned int length = strlen(s);
-	char num[4];
-	unsigned int i = 0;
-	unsigned int j = 0;
-	unsigned int k = 0;
-	unsigned int sign = 1;
-	unsigned int pow = 1;
-	unsigned int number = 0;
-	unsigned int num_length;
+	int start, end, i;
+	int startFound = 1;
+	int sign = 1;
+	int length = strlen(s);
+	int number = 0;
 
-	while (i <= length)
-	{	
-		if(s[i] == '-')
-			sign = sign * -1;	
-		
-		if(s[i] >= 48 && s[i] <= 57)
-		{	
-			num[j] = s[i];
-			j++;
-			if(s[i + 1] < 48 && s[i + 1] > 57)
+	for (i = 0; i < length; i++)
+	{
+		if (s[i] == '-')
+			sign = sign * -1;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			if (startFound)
+			{
+				start = i;
+				startFound = 0;
+			}
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+			{
+				end = i;
 				break;
+			}
 		}
-		i++;
-	}	
-		
-		num_length = strlen(num);
-		while (num_length > 1)
-		{
-			pow = pow * 10;
-			num_length--;
-		}
-		num_length = strlen(num);
-		while (k < num_length)
-		{
-			number = number + (num[k] - 48) * pow;
-			pow = pow / 10;
-			k++;
-		}
-	
-		number = number * sign;
-	
-	return number;
+	}
+	for (i = start; i <= end; i++)
+	{
+		number = number * 10 + (s[i] - '0');
+
+	}
+	number = number * sign;
+	return (number);
 }
--- pointers_arrays_strings/100-main.c
+++ pointers_arrays_strings/100-main.c
@@ -16,6 +16,8 @@ int main(void)
     printf("%d\n", nb);
     nb = _atoi("          ------++++++-----+++++--98");
     printf("%d\n", nb);
+    nb = _atoi("214748364");
+    printf("%d\n", nb);
     nb = _atoi("0");
     printf("%d\n", nb);
     nb = _atoi("Suite 402");
