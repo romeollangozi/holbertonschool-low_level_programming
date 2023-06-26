6716a6177f5218eae9786432b08aaf8d4cd03b63
romeollangozi <romeollangozi123@gmail.com>
2023-06-26 00:59:08 -0700

    0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *reset_to_98 - takes a pointer to an int as parameter and updates the value
+ *@n: pointer to the value to be updated
+ *
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
