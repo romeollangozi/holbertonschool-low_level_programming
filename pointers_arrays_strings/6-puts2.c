788f1684df5422bbe4e3ed8424c3d24d31fef688
romeollangozi <romeollangozi123@gmail.com>
2023-06-26 02:05:19 -0700

    6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ *puts2 - unction that prints every other character of a string,
+ *starting with the
+ *first character, followed by a new line
+ *@s: string
+ */
+void puts2(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i = i + 2;
+	}
+		_putchar('\n');
+}
