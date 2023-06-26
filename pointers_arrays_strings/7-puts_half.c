769c0522b8d7006f4d185b2215cbbe530297a0af
romeollangozi <romeollangozi123@gmail.com>
2023-06-26 03:07:18 -0700

    7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -8,13 +8,12 @@ void puts_half(char *str)
 {	int i;
 
 	if (strlen(str) % 2 == 0)
-		i = strlen(str) / 2;
+		i = (strlen(str) / 2) - 1;
 	else
 		i = (strlen(str) - 1) / 2;
 	while (str[i] != '\0')
-	{
+	{	i++;
 		_putchar(str[i]);
-		i++;
 	}
 		_putchar('\n');
 }
