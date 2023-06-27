39eb8adea5c9e18c5d7445324140f69557c63c88
romeollangozi <romeollangozi123@gmail.com>
2023-06-27 01:36:02 -0700

    1

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -11,11 +11,10 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int dest_length = strlen(dest);
 	int i;
-
-	for (i = 0; i < n; i++)
+	
+	for (i = 0; i < n && src[i] != 0; i++)
 	{
 		*(dest + dest_length + i) = src[i];
 	}
-
 	return (dest);
 }
