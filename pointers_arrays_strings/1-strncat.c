310d4b06173a16021193384219f25e5e14d55cae
romeollangozi <romeollangozi123@gmail.com>
2023-06-27 01:29:09 -0700

    1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strncat - concatenates two trings
+ * @dest: param
+ * @src: param
+ * @n: n bytes
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_length = strlen(dest);
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(dest + dest_length + i) = src[i];
+	}
+
+	return (dest);
+}
