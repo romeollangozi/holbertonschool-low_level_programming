09cc62ca40a054f6bca2032636e50a419472a811
romeollangozi <romeollangozi123@gmail.com>
2023-06-27 04:04:16 -0700

    3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,27 +1,21 @@
 #include "main.h"
 #include <string.h>
 /**
- *_strncmp -  function that copies a string
+ *_strcmp -  function that copies a string
  *@s1: first string to be compared
  *@s2: string to be compared with s1
  *Return: returns an integer indicating the result of the
  *comparison
 */
 int _strcmp(char *s1, char *s2)
-{	
+{
 	int i;
 
-	if (strlen(s1) != strlen(s2))
-		return (s1 - s2);
-	else
+	for (i = 0; i < (int) strlen(s1); i++)
 	{
-		for(i = 0; i < (int) strlen(s1); i++)
-		{
-			if (s1[i] != s2[i])
-				return (-1);
-
-		}
-			return (0);
+		if (s1[i] - s2[i] != 0)
+			return (s1[i] - s2[i]);
 	}
+	return (0);
 
 }
