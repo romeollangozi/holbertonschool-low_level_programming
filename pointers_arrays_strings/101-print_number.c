b6f99a20fc0ec8f6e84571925494cf5001db5b04
romeollangozi <romeollangozi123@gmail.com>
2023-07-01 07:16:46 -0700

    101

--- pointers_arrays_strings/101-print_number.c
+++ pointers_arrays_strings/101-print_number.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdlib.h>
 /**
  * print_number - prints an integer
  *@n: integer
@@ -12,22 +12,21 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		n = n * -1;
 	}
 
-	while (n / divisor > 9)
+	while (abs(n / divisor) > 9)
 		divisor = divisor * 10;
 
 	while (divisor > 0)
 	{
-		if (divisor > n)
+		if (divisor > abs(n))
 		{
 			_putchar('0');
 			divisor = divisor / 10;
 		}
 		else
 		{
-			_putchar('0' + n / divisor);
+			_putchar('0' + abs(n / divisor));
 			n = n - (n / divisor) * divisor;
 			divisor = divisor / 10;
 		}
