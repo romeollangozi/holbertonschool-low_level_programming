2392304ffebc56b45518f175cbe5bf09f2b7de25
romeollangozi <romeollangozi123@gmail.com>
2023-07-05 03:02:36 -0700

    0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: number of argumenst supplied to argv
+ * @argv: vectorial array of arguments
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
