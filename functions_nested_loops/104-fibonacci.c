69b2d4d3d4912fede978fa8d82e5fc194770de78
romeollangozi <romeollangozi123@gmail.com>
2023-07-05 15:09:16 -0700

    s

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -9,24 +9,20 @@
 int main(void)
 {
 int i;
-long int a = 1;
-long int b = 1;
-long int t;
+unsigned long long int a = 1;
+unsigned long long int b = 2;
+unsigned long long int t;
 int n = 10;
 printf("1, 2, ");
-for (i = 0; i <= 98; i++)
+for (i = 0; i <= 94; i++)
 {
 t = b;
 b = b + a;
 a = t;
-if (b >= 10)
-{
-while (b / n != 1 && b >= 10)
-n = n * 10;
-}
-if (b / n == 1 || b / n == 2)
-printf("%li, ", b);
-n = 10;
+if (i != 98)
+printf("%llu, ", b);
+else
+printf("%llu", b);
 }
 printf("\n");
 return (0);
---
+++ hello_world/betty.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+   int a;
+   int b = 5;  
+for (a = 0; a < b; a++)
+{
+    printf("%d\n", a);
+}
+
+
+  return (0);
+}
