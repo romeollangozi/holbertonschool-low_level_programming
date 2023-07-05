1c824c13722f193d32fc0a83a564bc477b0ad10f
romeollangozi <romeollangozi123@gmail.com>
2023-07-05 03:14:49 -0700

    2

---
+++ argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: number of argumenst supplied to argv
+ * @argv: vectorial array of arguments
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (i < argc)
+	{
+	printf("%s\n", argv[i]);
+	i++;
+	}
+	return (0);
+}
