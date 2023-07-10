c05b80b0bd5ed93649ac1aa23feeb1a214d27f3a
romeollangozi <romeollangozi123@gmail.com>
2023-07-10 03:26:33 -0700

    s

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -13,9 +13,13 @@ char *str_concat(char *s1, char *s2)
 	size_t len;
 	char *new;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
 	{
-		return (NULL);
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
 	}
 	len = strlen(s1) + strlen(s2);
 	new = malloc(len + 1);
