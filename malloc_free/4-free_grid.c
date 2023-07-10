e5d40d160774e86cc7dde5ce2d58f4a5852c544f
romeollangozi <romeollangozi123@gmail.com>
2023-07-10 05:04:45 -0700

    s

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <string.h>
+/**
+ * free_grid - returns a pointer to a 2 dimensional array
+ *@grid: width of the array
+ *@height: height of the array
+ */
+void free_grid(int **grid, int height)
+{
+		int i;
+
+		for (i = 0; i < height; i++)
+		{
+			free(grid[i]);
+		}
+		free(grid);
+}
