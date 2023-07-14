4dd65c7b8ced0f70f7f536ba6452a042eca17c34
romeollangozi <romeollangozi123@gmail.com>
2023-07-14 06:18:39 -0700

    s

---
+++ malloc_free/101-strtow.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * strtow - function that splits a string into word
+ * @str: string to be splited
+ * Return: pointer to an array of strings
+ */
+
+char **strtow(char *str)
+{
+	char **array;
+	int i;
+	int k = 0, j = 0;
+	int wNum = 0;
+
+	if (str == NULL || !(strcmp(str, "")))
+		return (NULL);
+	for (i = 0; i < (int) strlen(str); i++)
+		if (str[i] != ' ' && str[i + 1] == ' ')
+			wNum++;
+	array = malloc(sizeof(char *) * (wNum + 1));
+	if (array == NULL)
+		return (NULL);
+	for (i = 0; i <= wNum; i++)
+		array[i] = malloc(50);
+	for (; i >= 0; i--)
+	{
+		if (array[i] == NULL)
+			free(array[i]);
+	}
+	for (i = 0; i < (int) strlen(str); i++)
+	{
+		if (str[i] != ' ')
+		{
+			array[j][k] = str[i];
+			k++;
+			if (str[i + 1] == ' ')
+			{
+				array[j][k] = '\0';
+				j++;
+				k = 0;
+			}
+		}
+	}
+	array[j] = NULL;
+	return (array);
+}
