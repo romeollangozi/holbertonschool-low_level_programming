91a3e9ab448ededd4f10536eab0cc830202bdfd9
romeollangozi <romeollangozi123@gmail.com>
2023-07-17 07:43:51 -0700

    all

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -12,6 +12,6 @@ void print_dog(struct dog *d)
 	{
 		d->name == NULL ? printf("Name: (nil)\n") : printf("Name: %s\n", d->name);
 		printf("Age: %f\n", d->age);
-		d->owner == NULL ? printf("Owner: (nil)\n") : printf("Name: %s\n", d->owner);
+		d->owner == NULL ? printf("Owner: (nil)\n") : printf("Owner: %s\n", d->owner);
 	}
 }
