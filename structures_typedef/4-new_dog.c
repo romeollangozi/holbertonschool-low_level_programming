03dc9dafeacb9e797752a42901d021344e115701
romeollangozi <romeollangozi123@gmail.com>
2023-07-17 08:06:34 -0700

    m

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
+#include <string.h>
 /**
  *new_dog - creates a new dog instance
  *@name: name of the dog
@@ -11,12 +12,16 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	char *Cname = name;
-	char *Cowner = owner;
+	char *Cname = strdup(name);
+	char *Cowner = strdup(owner);
 	dog_t *newdog = malloc(sizeof(dog_t));
 
 	if (newdog == NULL)
-	return (NULL);
+	{
+		free(newdog);
+		return (NULL);
+	}
+
 	newdog->name = Cname;
 	newdog->owner = Cowner;
 	newdog->age = age;
