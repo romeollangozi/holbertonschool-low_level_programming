ca4a4cf6b77495cce46afe8d603d0a34cb6bffeb
romeollangozi <romeollangozi123@gmail.com>
2023-07-17 08:09:02 -0700

    m

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -16,8 +16,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	char *Cowner = strdup(owner);
 	dog_t *newdog = malloc(sizeof(dog_t));
 
-	if (newdog == NULL)
+	if (newdog == NULL || Cowner == NULL || Cname == NULL)
 	{
+		free(Cowner);
+		free(Cname);
 		free(newdog);
 		return (NULL);
 	}
