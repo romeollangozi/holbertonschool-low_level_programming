8f3ec8c09c5181de78bb62a139cfb2d30e8b2fa8
romeollangozi <romeollangozi123@gmail.com>
2023-07-17 13:53:51 -0700

    6

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - free memory allocated by a dog struct
+ * @d: dog pointer
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
