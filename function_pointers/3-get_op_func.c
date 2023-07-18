7c524e609029068de84eaf33fd7768ac14d9fde9
romeollangozi <romeollangozi123@gmail.com>
2023-07-18 05:35:18 -0700

    m

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -19,9 +19,9 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL},
 	};
-	int i;
+	int i = 0;
 
-	while (i < 6)
+	while (ops[i].op)
 	{
 		if (strcmp(ops[i].op, s) == 0)
 			return (ops[i].f);
