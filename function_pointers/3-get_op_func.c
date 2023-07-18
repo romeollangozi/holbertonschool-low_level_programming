6a0d10f9e0b5e4ef3dcc461845732e9c11059222
romeollangozi <romeollangozi123@gmail.com>
2023-07-18 05:24:05 -0700

    m

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,6 +1,7 @@
 #include "3-calc.h"
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 /**
  *get_op_func - function that selects the correct function
  *to perform the operation asked by the user
@@ -22,9 +23,11 @@ int (*get_op_func(char *s))(int, int)
 
 	while (i < 6)
 	{
-		if (strcmp(ops[i].op,s) == 0)
+		if (strcmp(ops[i].op, s) == 0)
 			return (ops[i].f);
 		i++;
 	}
+	printf("Error\n");
+	exit(99);
 	return (NULL);
 }
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -5,32 +5,24 @@
 #include <stddef.h>
 /**
  *main - check the code
+ *@argc: arguments count
+ *@argv: argument list
  *Return: Always 0.
  */
 
 int main(int argc, char *argv[])
-{	
+{
 	int result;
-	int (*function)(int,int);
-	
+	int (*function)(int, int);
+
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	/*if (((strcmp(argv[2], "/") == 0) || (strcmp(argv[2], "*") == 0)) &&
-			(argv[1] == 0 || argv[3] == 0))
-	{
-		printf("Error\n");
-		exit(100);
-	}*/
-	function= get_op_func(argv[2]);
-	if (*function == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-	result = function(atoi(argv[1]),atoi(argv[3]));
-	printf("%d\n",result);
+
+	function = get_op_func(argv[2]);
+	result = function(atoi(argv[1]), atoi(argv[3]));
+	printf("%d\n", result);
 	return (0);
 }
