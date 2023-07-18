c67f31e0b8ae3feb61950301c297d37fb1c26f68
romeollangozi <romeollangozi123@gmail.com>
2023-07-18 15:10:49 -0700

    code indent correction

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -15,7 +15,8 @@ int sum_them_all(const unsigned int n, ...)
 	if (n == 0)
 		return (0);
 	for (i = 0; i < n; i++)
-	 sum += va_arg(args, int);
-
+	{
+		sum += va_arg(args, int);
+	}
 	return (sum);
 }
