705e86ca55535e85db629876761d8ebcd1a1c5ea
romeollangozi <romeollangozi123@gmail.com>
2023-07-18 02:31:50 -0700

    m

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -11,7 +11,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i;
 
-	if (action != NULL)
+	if (action != NULL || arrray != NULL)
 	{
 		for (i = 0; i < (int) size; i++)
 			action(array[i]);
