21f2e24b8591c8ee427cddd4807ab7d79af97532
romeollangozi <romeollangozi123@gmail.com>
2023-07-18 15:42:13 -0700

    handled NULL parameter"

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -12,12 +12,16 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	va_start(args, n);
-	if (n != 0 || separator != NULL)
+	if (n != 0)
 	{
 	for (i = 0; i < n - 1; i++)
 	{
-		printf("%d%s ", va_arg(args, int), separator);
+		if (separator != NULL)
+			printf("%d%s", va_arg(args, int), separator);
+		else
+			printf("%d", va_arg(args, int));
 	}
-		printf("%d\n", va_arg(args, int));
+		printf("%d", va_arg(args, int));
 	}
+	printf("\n");
 }
