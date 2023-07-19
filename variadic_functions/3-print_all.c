374680fe193e7926d0535552c1d76eed7e24a2c5
romeollangozi <romeollangozi123@gmail.com>
2023-07-19 07:10:10 -0700

    handled NULL assignment to string fromat

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -11,13 +11,13 @@ void print_all(const char * const format, ...)
 	int i = 0;
 	char *t;
 	char *separator = ", ";
-	int length = strlen(format);
+	int length = format == NULL ? 0 : strlen(format);
 
+	if (length != 0)
+	{
 	va_start(args, format);
 	while (format[i] != '\0')
 	{
-		if (i == length - 1)
-			separator = "";
 		switch (format[i])
 		{
 			case 's':
@@ -27,21 +27,22 @@ void print_all(const char * const format, ...)
 					printf("%s%s", t, i == length - 1 ? "" : separator);
 					break;
 				}
-					printf("(nil)%s", separator);
+					printf("(nil)%s", i == length - 1 ? "" : separator);
 					break;
 			case 'i':
-				printf("%d%s", va_arg(args, int), separator);
+				printf("%d%s", va_arg(args, int), i == length - 1 ? "" : separator);
 				break;
 			case 'f':
-				printf("%f%s", va_arg(args, double), separator);
+				printf("%f%s", va_arg(args, double), i == length - 1 ? "" : separator);
 				break;
 			case 'c':
-				printf("%c%s", va_arg(args, int), separator);
+				printf("%c%s", va_arg(args, int), i == length - 1 ? "" : separator);
 				break;
 			default:
 				break;
 		}
 		i++;
 	}
+		}
 		printf("\n");
 }
