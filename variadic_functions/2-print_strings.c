76a89a6f6331cb22d84025799e599cea69733726
romeollangozi <romeollangozi123@gmail.com>
2023-07-19 02:18:55 -0700

    corrected invocation problem of function va_arg"

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -10,18 +10,32 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i;
+	char *t;
 
 	va_start(args, n);
 	if (n != 0)
 	{
 	for (i = 0; i < n - 1; i++)
 	{
+		t = va_arg(args, char *);
 		if (separator != NULL)
-			printf("%s%s", va_arg(args, char *), separator);
+		{
+			if (t != NULL)
+				printf("%s%s", t, separator);
+			else
+			{
+				printf("(nil)%s", separator);
+			}
+
+		}
 		else
-			printf("%s", va_arg(args, char*));
+			printf("%s", t);
 	}
-			printf("%s", va_arg(args, char *));
+		t = va_arg(args, char *);
+		if (t != NULL)
+			printf("%s", t);
+		else
+			printf("(nil)%s", separator);
 	}
 	printf("\n");
 }
