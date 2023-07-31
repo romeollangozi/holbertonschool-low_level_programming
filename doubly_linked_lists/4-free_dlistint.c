87e4707584214a86d994669b6a49748177626109
romeollangozi <romeollangozi123@gmail.com>
2023-07-31 12:26:32 -0700

    function that frees all memory allocated to a linked list

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_dlistint - function that frees a doubly linked list
+ * @head: head node
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	if (head->next != NULL)
+		free_dlistint(head->next);
+	free(head);
+}
