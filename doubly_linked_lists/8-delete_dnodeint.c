3fbdaa0572c69f5319a331fa12e8bf4d70cf59f8
romeollangozi <romeollangozi123@gmail.com>
2023-08-01 03:35:03 -0700

    error fixed

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -17,6 +17,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (index == 0 && it != NULL)
 	{
 		(*head) = (*head)->next;
+		it->next->prev = NULL;
 		return (1);
 	}
 	if (it == NULL)
