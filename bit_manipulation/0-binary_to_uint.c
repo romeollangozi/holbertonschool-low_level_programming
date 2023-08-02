875150c5dd7e18a2f53b41c184bffc999781aa58
romeollangozi <romeollangozi123@gmail.com>
2023-08-02 07:37:03 -0700

    handled NULL argument

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -19,7 +19,7 @@ unsigned int _pow(unsigned int base, unsigned int power)
 }
 
 /**
- *binary_to_int - function that converts a binary to an unsigned int
+ *binary_to_uint - function that converts a binary to an unsigned int
  *@b: binary
  *Return: the converted number or 0 if there is one or more chars in the\
  *string b that is not 0 or 1 and if b is NULL
@@ -29,11 +29,11 @@ unsigned int binary_to_uint(const char *b)
 {
 	int i = 0;
 	unsigned int decimal = 0;
-	int length = strlen(b) - 1;
+	int length;
 
 	if (b == NULL)
 		return (0);
-
+	length = strlen(b) - 1;
 	while (b[i] != '\0')
 	{
 		if (b[i] != '0' && b[i] != '1')
