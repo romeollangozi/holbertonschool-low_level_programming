7711a5df0e0be5340714e2839a22f18c9fb53af7
romeollangozi <romeollangozi123@gmail.com>
2023-08-02 08:20:20 -0700

    function that prints a binary representation of a decimal number using only bitwise operators

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_binary - function that prints a binary representation of
+ *a decimal number
+ *@n: number
+ */
+
+void print_binary(unsigned long int n)
+{
+	if ((n >> 1) == 0)
+	{
+		_putchar('0' + (n & 1));
+	}
+	else
+	{
+		print_binary(n >> 1);
+		_putchar('0' + (n & 1));
+	}
+} 
--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 unsigned int binary_to_uint(const char *b);
-unsigned int _pow(unsigned int base, unsigned int power);
+void print_binary(unsigned long int n);
+int _putchar(char c);
 #endif
