9e592deeeca1698dece9d8e3e77e942f77132f2d
romeollangozi <romeollangozi123@gmail.com>
2023-08-02 07:35:12 -0700

    betty documentation fix

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -6,11 +6,11 @@
 /**
  * _pow - function that returns a number to power n
  *@base: the base of the number
- *@power: power 
+ *@power: power
  *Return: number to the power n
  */
 
-unsigned int _pow(unsigned int base, unsigned power)
+unsigned int _pow(unsigned int base, unsigned int power)
 {
 	if (power == 0)
 		return (1);
@@ -29,8 +29,8 @@ unsigned int binary_to_uint(const char *b)
 {
 	int i = 0;
 	unsigned int decimal = 0;
-	int length = strlen(b) -1;
-	
+	int length = strlen(b) - 1;
+
 	if (b == NULL)
 		return (0);
 
@@ -38,7 +38,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		
+
 		if (b[i] == '1')
 		{
 			decimal += _pow(2, length);
