166993daa2469cc6ca500de50da8e72ace68aa5d
romeollangozi <romeollangozi123@gmail.com>
2023-08-07 03:27:34 -0700

    function that creates a new file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * create_file - function that creates a text file
+ * @filename: name of the file
+ * @text_content: text content
+ * Return: 1 on succes, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int length = strlen(text_content);
+	int n;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	n = write(fd, text_content, length);
+	if (n == -1)
+		return (-1);
+	
+	return (1);
+}
