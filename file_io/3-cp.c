4b8a0f4ee079ba6941903c14b3f6015a075bff98
romeollangozi <romeollangozi123@gmail.com>
2023-08-07 08:27:32 -0700

    helper function

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -6,6 +6,24 @@
 #include <stdlib.h>
 
 /**
+ * closefd - closes file descriptors
+ *@fd1: first file
+ *@fd2: second file
+ */
+void closefd(int fd1, int fd2)
+{
+	if (close(fd1) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd1);
+	}
+	if (close(fd2) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd2);
+	}
+}
+
+
+/**
  * main - Entry point
  * @argc: number of argumenst supplied to argv
  * @argv: vectorial array of arguments
@@ -20,33 +38,33 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	fdr = open(argv[1], O_RDONLY);
 	fdw = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
-
 	if (fdr == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if (fdr == -1)
+	if (fdw == -1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-	do {
 		n = read(fdr, buffer, 1024 * 8);
+		if (n == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
 		m = write(fdw, buffer, n);
 		if (m == -1)
 		{
-			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
-	} while (n == 1024 * 8);
-	close(fdr);
-	close(fdw);
+		closefd(fdr, fdw);
 	return (0);
 }
