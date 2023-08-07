66219576859dd8d5e353cd6befe6573a36755552
romeollangozi <romeollangozi123@gmail.com>
2023-08-07 03:30:55 -0700

    handled null text content

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	int length = strlen(text_content);
 	int n;
-
+	char *empty = "";
 	if (filename == NULL)
 		return (-1);
 
@@ -25,7 +25,10 @@ int create_file(const char *filename, char *text_content)
 
 	if (fd == -1)
 		return (-1);
-
+	if (text_content == NULL)
+	{
+		n = write(fd, empty, 0);
+	}
 	n = write(fd, text_content, length);
 	if (n == -1)
 		return (-1);
