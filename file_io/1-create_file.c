0c43cb5932af84b5fb9c8fa051f75bdd6961b618
romeollangozi <romeollangozi123@gmail.com>
2023-08-07 03:49:57 -0700

    1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	int length;
-	int n;
+	int n = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -26,9 +26,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (text_content == NULL)
-		n = write(fd, "", 1);
-	else
+	if (text_content != NULL)
 	{
 		length = strlen(text_content);
 		n = write(fd, text_content, length);
