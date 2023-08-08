5f962f4be7581b3e30643d2006e0602283e423ac
romeollangozi <romeollangozi123@gmail.com>
2023-08-08 04:05:50 -0700

    3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -12,25 +12,43 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	char *nKey = strdup(key);
-	char *nValue = strdup(value);
+	char *nKey;
+	char *nValue;
 	unsigned long int index;
 	hash_node_t *temp = NULL;
+	hash_node_t *it = NULL;
 	hash_node_t *newNode = malloc(sizeof(hash_node_t));
+	int found = 0;
 
-	if (newNode == NULL)
+	if (newNode == NULL || key == NULL || value == NULL)
 		return (0);
+	nKey = strdup(key);
+	nValue = strdup(value);
 	newNode->key = nKey;
 	newNode->value = nValue;
 	index = key_index((unsigned char *)key, ht->size);
-
 	if (ht->array[index] == NULL)
+	{
 		ht->array[index] = newNode;
+	}
 	else
 	{
-		temp = ht->array[index];
-		ht->array[index] = newNode;
-		ht->array[index]->next = temp;
+		it = ht->array[index];
+		while (it != NULL)
+		{
+			if (strcmp(it->key, key) == 0)
+			{
+				it->value = newNode->value;
+				found = 1;
+			}
+			it = it->next;
+		}
+		if (!found)
+		{
+			temp = ht->array[index];
+			ht->array[index] = newNode;
+			ht->array[index]->next = temp;
+		}
 	}
 	return (1);
 }
