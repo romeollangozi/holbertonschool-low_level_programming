5f7bb9d740769143422a50d4dd1293ff4d29943c
romeollangozi <romeollangozi123@gmail.com>
2023-08-08 04:06:49 -0700

    3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -20,7 +20,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *newNode = malloc(sizeof(hash_node_t));
 	int found = 0;
 
-	if (newNode == NULL || key == NULL || value == NULL)
+	if (ht == NULL || newNode == NULL || key == NULL || value == NULL)
 		return (0);
 	nKey = strdup(key);
 	nValue = strdup(value);
