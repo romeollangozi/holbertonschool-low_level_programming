0381f433409f19fca4491ddc8df1a8674e003067
romeollangozi <romeollangozi123@gmail.com>
2023-09-14 02:27:30 -0700

    linear

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -18,6 +18,7 @@ int linear_search(int *array, size_t size, int value)
 		return (-1);
 	for (i = 0; i < (int) size; i++)
 	{
+		printf("Value checked array[%i] = [%d]\n", i, array[i]);
 		if (*(array + i) == value)
 			return (i);
 	}
